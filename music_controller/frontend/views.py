c2a1b54bb52d0c2354e2d15f53740975e22c1d8c
eli <ellie.tms1371@gmail.com>
2021-09-01 16:12:51 +0100

    frontend setup urls

--- music_controller/frontend/views.py
+++ music_controller/frontend/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+# render our html and let react take care of it
+def index(request, *args, **kwargs):
+    return render(request, 'frontend/index.html')
+
+
