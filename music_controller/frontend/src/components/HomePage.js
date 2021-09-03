4d74d62b2f518412e984eb19ab4e7d767bdb146c
eli <ellie.tms1371@gmail.com>
2021-09-03 12:48:08 +0100

    text changes

--- music_controller/frontend/src/components/HomePage.js
+++ music_controller/frontend/src/components/HomePage.js
@@ -1,15 +1,12 @@
-import App from "./App"
-import React , {Component} from "react"
-import { render } from "react-dom"
+import App from "./App";
+import React, { Component } from "react";
+import { render } from "react-dom";
 
-
-export default class HomePage extends Component{
-    constructor(props){
-        super(props)
-    }
-    render(){
-        return(
-            <strong>Helloo World!</strong>
-        )
-    }
+export default class HomePage extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return <strong> Helloo All!</strong>;
+  }
 }
