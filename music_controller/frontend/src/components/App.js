6d7926402ea6d6f0627495e1d28b1f16c6ae649f
eli <ellie.tms1371@gmail.com>
2021-09-03 12:49:36 +0100

    rendering pages to app.js

--- music_controller/frontend/src/components/App.js
+++ music_controller/frontend/src/components/App.js
@@ -1,23 +1,24 @@
-import React, {Component} from "react"
-import {render} from "react-dom"
-import HomePage from "./HomePage"
+import React, { Component } from "react";
+import { render } from "react-dom";
+import HomePage from "./HomePage";
+import CreateRoom from "./CreateRoom";
+import RoomJoinPage from "./RoomJoinPage";
 
-export default class App extends Component{
-    constructor(props){
-        super(props)
-    }
-    render() { 
-        return (
-            <div>
-                <h1>
-                   Ellie is Testing 😅 
-                </h1>
-                <HomePage/>
-            </div>    
-        )
-    }
-}    
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h1>Ellie is Testing 😅</h1>
+        <HomePage />
+        <CreateRoom />
+        <RoomJoinPage />
+      </div>
+    );
+  }
+}
 
-
-const divContainer = document.getElementById("app")
-render(<App/> , divContainer)
+const divContainer = document.getElementById("app");
+render(<App />, divContainer);
